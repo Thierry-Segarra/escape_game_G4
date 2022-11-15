340794472df21e50da04122e1eeabb9185fb944c
Alex78874 <garibaldialexis@gmail.com>
2022-11-15 15:39:31 +0100

    C'est presque fini!
    oui!

--- stage_2/script.js
+++ stage_2/script.js
@@ -3,6 +3,19 @@ var div;
 
 var isDown = false;
 
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+function changeDocument(){
+    var item_elem = document.getElementsByClassName('item');
+for (var i = 0; i < item_elem.length; ++i) {
+    var item = item_elem[i];  
+    skin = randomIntFromInterval(1,6);
+    item.style.backgroundImage = "url(images/document_" + skin + ".png)"
+}
+}
+
 // Drag and drop //
 
 document.addEventListener('mousedown', function(e) {
@@ -16,11 +29,11 @@ document.addEventListener('mousedown', function(e) {
 
 document.addEventListener('mouseup', function(e) {
     isDown = false;
-    if (document.elementFromPoint(e.clientX, e.clientY).className === "box"){
+    if ((document.elementFromPoint(e.clientX, e.clientY).classList.contains('box')) && div!=null){
         box = document.elementFromPoint(e.clientX, e.clientY);
         div.style.zIndex = 2;
 
-        box.appendChild(div);
+        box.prepend(div);
         div.style.position = "static";
         div = null;
     }
@@ -28,7 +41,7 @@ document.addEventListener('mouseup', function(e) {
 
 document.addEventListener('mousemove', function(event) {
     event.preventDefault();
-    if (isDown) {
+    if (isDown && (div != null)) {
         mousePosition = {
 
             x : event.clientX,
@@ -40,4 +53,17 @@ document.addEventListener('mousemove', function(event) {
     }
 }, true);
 
-// Verification des phrases //
+// Verification des taches //
+
+function verification(){
+    box_1 = document.getElementById('box_1')
+    item_1 = document.getElementById('item_1')
+
+    if (box_1.contains(item_1)){
+        console.log("oui!")
+    }
+}
+
+function textTerminal(){
+
+}
