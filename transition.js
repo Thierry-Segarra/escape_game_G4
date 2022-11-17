60dcbd5753c7d7cdde3906d05a18a69467b42cc8
Nyxias130 <iagueniyanisi@gmail.com>
2022-11-17 14:47:59 +0100

    added better transition event

--- transition.js
+++ transition.js
@@ -1,8 +1,10 @@
-function transitionFermer(text,lien) {
+function transitionFermer() {
   document.getElementById("transition").style.marginLeft = "-100px";
   document.getElementById("text").innerHTML = '<textarea class="area" id="textdinamique" disabled></textarea>';
   let div = 'textdinamique';
-  let suivant = '<a class="bouton" href="'+lien+'">Au Boulot</a>';
+  let text = `Surprenant ! La phase de cadrage ne vous a pas vu venir.
+Sans ces livrables, le projet se dirigerait droit dans le mur !`;
+  let suivant = '<a class="bouton" href="stage_2.php">Suivant</a>';
   let bouton = 'text';
   setTimeout(textDinamique,3000,div,bouton, text,suivant)
 }
@@ -12,7 +14,9 @@ function transitionOuver() {
 var interval_text;
 var t = 0;
 
-
+textDinamique('textdinamique','text',`Le projet ne doit pas tomber à l'eau !
+Vous êtes notre dernier espoir, reprenez en main le projet et menez le jusqu'à sa réussite !
+Commencez par retrouver les livrables perdus et rejoignez votre équipe.`,'<button class="bouton" onclick = "transitionOuver()">Au boulot !</button>');
 
 function textDinamique(div,bouton, text,suivant) {
 var nb_text = text.length;
--- transition.php
+++ transition.php
@@ -1,7 +1,12 @@
 <head>
     <link rel="stylesheet" href="transition.css">
 </head>
+
+<button id="color" onclick="transitionFermer()">Fermer</button>
+<button id="color" onclick="transitionOuver()">Ouver</button>
+
 <div class="box" id="transition">
+    <div class="madame"></div>
     <p class="text" id="text"><textarea class="area" id="textdinamique" disabled></textarea></p>
 </div>
 
