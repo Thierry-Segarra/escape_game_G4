1b9580420b8ee87b7de2790081e0033ac4a31784
JCH7 <chenhphi@gmail.com>
2022-11-19 21:12:33 +0100

    bdd host

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-$db_username = 'root';
-$db_password = '';
-$db_name     = 'escape_game_g4';// verifier ici le nom de la base de donnée
-$db_host     = 'localhost';
+$db_username = 'jch';
+$db_password = 'limdp2444';
+$db_name     = 'jch_gscape';// verifier ici le nom de la base de donnée
+$db_host     = 'mysql-jch.alwaysdata.net';
 $db = mysqli_connect($db_host, $db_username, $db_password,$db_name) or die('could not connect to database');
 ?>
